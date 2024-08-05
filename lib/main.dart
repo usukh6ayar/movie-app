0975e7fb4522beb5271b6faacaa5e8f1f0de0900
usukh6ayar <usukhbayareazy@gmail.com>
2024-08-05 19:12:45 +0800

    structure done

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movie/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie App',
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:movie/screens/movies.dart';
+import 'package:movie/screens/profile.dart';
+import 'package:movie/screens/wishlist.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _totalPages = [
+    MoviesPage(),
+    WishListPage(),
+    ProfilePage(),
+  ];
+
+  void _setCurrentIndex(int val) {
+    setState(() {
+      _currentIndex = val;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _totalPages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _setCurrentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.movie), label: "Кино"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Дуртай"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Профайл"),
+        ],
+      ),
+    );
+  }
+}
